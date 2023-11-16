400ec16b916361fa6d2140b06811292370c7889d
bittoboyo <89293051+bittoboyo@users.noreply.github.com>
2023-11-17 09:25:12 +1100

    Created Initial Project
    Let's get this show on the road! >:D

---
+++ SherpaApp/SherpaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SherpaAppApp.swift
+//  SherpaApp
+//
+//  Created by Marell Bito on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SherpaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
