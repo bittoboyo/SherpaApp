2e04a46d78f3b85eb4309929dac1478fc9b1c323
Sunny Wong <sunnywong0207@gmail.com>
2023-11-23 10:54:05 +1100

    New View files created

---
+++ SherpaApp/EpicCoffeeView.swift
@@ -0,0 +1,18 @@
+//
+//  EpicCoffeeView.swift
+//  SherpaApp
+//
+//  Created by Yu Ching Wong on 23/11/2023.
+//
+
+import SwiftUI
+
+struct EpicCoffeeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpicCoffeeView()
+}
---
+++ SherpaApp/SydneyLocalLibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  SydneyLocalLibraryView.swift
+//  SherpaApp
+//
+//  Created by Yu Ching Wong on 23/11/2023.
+//
+
+import SwiftUI
+
+struct SydneyLocalLibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SydneyLocalLibraryView()
+}
