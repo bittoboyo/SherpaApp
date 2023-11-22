f72c5f22f2b9945ed3e7eff4b8f9132b606cd01d
alveralverr <34881987+alveralverr@users.noreply.github.com>
2023-11-22 13:27:11 +1100

    Created Unsave Popup

--- SherpaApp/SavedView.swift
+++ SherpaApp/SavedView.swift
@@ -1,111 +1,163 @@
 import SwiftUI
 
 struct SavedView: View {
+    @State var isUnsavePopupVisible = true
     var body: some View {
-        @State var isBookmarked = false
         ScrollView {
-            VStack {
-                HStack {
-                    Text("Saved Contacts")
-                        .font(.title)
-                        .fontWeight(.bold)
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("Saved People")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .frame(maxWidth: 360, alignment: .leading)
+                            .padding(.top, 20)
+                    }
+                    Divider()
+                        .padding(.bottom, 10)
+                    
+                    Text("Recently Added")
+                        .font(.title3)
                         .frame(maxWidth: 360, alignment: .leading)
-                        .padding(.top, 20)
+                    
+                    savedPerson(
+                        profilePhoto: "John",
+                        name: "John Smith",
+                        jobTitle: "Interaction Designer",
+                        company: "Apple",
+                        industry: "IT"
+                    )
+                    savedPerson(
+                        profilePhoto: "MaryP",
+                        name: "Mary Pops",
+                        jobTitle: "UX Researcher",
+                        company: "Microsoft",
+                        industry: "IT"
+                    )
+                    savedPerson(
+                        profilePhoto: "EvaM",
+                        name: "Eva Miller",
+                        jobTitle: "Data Scientist",
+                        company: "Amazon",
+                        industry: "IT"
+                    )
+                    savedPerson(
+                        profilePhoto: "EvaM",
+                        name: "Eva Miller",
+                        jobTitle: "Data Scientist",
+                        company: "Amazon",
+                        industry: "IT"
+                    )
+                    savedPerson(
+                        profilePhoto: "EvaM",
+                        name: "Eva Miller",
+                        jobTitle: "Data Scientist",
+                        company: "Amazon",
+                        industry: "IT"
+                    )
+                    savedPerson(
+                        profilePhoto: "EvaM",
+                        name: "Eva Miller",
+                        jobTitle: "Data Scientist",
+                        company: "Amazon",
+                        industry: "IT"
+                    )
                 }
-                Divider()
-                    .padding(.bottom, 10)
-                
-                Text("Recently Added")
-                    .font(.title3)
-                    .frame(maxWidth: 360, alignment: .leading)
-                
-                savedPerson(
-                    profilePhoto: "John",
-                    name: "John Smith",
-                    jobTitle: "Interaction Designer",
-                    company: "Apple",
-                    industry: "IT"
-                )
-                savedPerson(
-                    profilePhoto: "MaryP",
-                    name: "Mary Pops",
-                    jobTitle: "UX Researcher",
-                    company: "Microsoft",
-                    industry: "IT"
-                )
-                savedPerson(
-                    profilePhoto: "EvaM",
-                    name: "Eva Miller",
-                    jobTitle: "Data Scientist",
-                    company: "Amazon",
-                    industry: "IT"
-                )
-                savedPerson(
-                    profilePhoto: "EvaM",
-                    name: "Eva Miller",
-                    jobTitle: "Data Scientist",
-                    company: "Amazon",
-                    industry: "IT"
-                )
-                savedPerson(
-                    profilePhoto: "EvaM",
-                    name: "Eva Miller",
-                    jobTitle: "Data Scientist",
-                    company: "Amazon",
-                    industry: "IT"
-                )
-                savedPerson(
-                    profilePhoto: "EvaM",
-                    name: "Eva Miller",
-                    jobTitle: "Data Scientist",
-                    company: "Amazon",
-                    industry: "IT"
-                )
+//                .background(
+//                    Color.black.opacity(0.3).edgesIgnoringSafeArea(.all)
+//                    .onTapGesture {
+//                        isUnsavePopupVisible = false
+//                    }
+//                )
+                if isUnsavePopupVisible {
+                    UnsavePopup {
+                        isUnsavePopupVisible.toggle()
+                        print("User Unsaved")
+                    } onCancel: {
+                        isUnsavePopupVisible.toggle()
+                        print("Unsave Cancelled")
+                    }
+                }
             }
         }
     }
 }
 
 func savedPerson (profilePhoto: String, name: String, jobTitle: String, company: String, industry: String) -> some View {
-    @State var isBookmarked = false
-     return ZStack {
+    @State var isUnsavePopupVisible = true
+    return ZStack {
         RoundedRectangle(cornerRadius: 20)
-             .stroke(.gray, lineWidth: 1)
-             .fill(Color.white)
-             .frame(width: 370, height: 150)
-         HStack {
-             Image(profilePhoto)
-                 .resizable()
-                 .scaledToFit()
-                 .frame(width: 100)
-                 .clipShape(Circle())
-             VStack {
-                 Text(name)
-                     .font(.title2)
-                     .fontWeight(.bold)
-                     .frame(maxWidth: 170, alignment: .leading)
-                 Text(jobTitle + "\n" + "at " + company)
-                     .font(.subheadline)
-                     .frame(maxWidth: 170, alignment: .leading)
-                     .padding(.bottom, 5)
-                 Text(industry)
-                     .font(.subheadline)
-                     .fontWeight(.bold)
-                     .foregroundColor(.white)
-                     .padding(.horizontal, 20)
-                     .background(.blue)
-                     .cornerRadius(5)
-                     .frame(maxWidth: 170, alignment: .leading)
-             }
-             Toggle(isOn: $isBookmarked) {
-                 Image(systemName: isBookmarked ? "bookmark.fill" : "bookmark")
-                     .font(.system(size: 30))
-             }
-                    .toggleStyle(.button)
-                    .clipShape(Circle())
-                    .offset(x: 10, y: -25)
-                    .padding(0)
-         }
+            .stroke(.gray, lineWidth: 1)
+            .fill(Color.white)
+            .frame(width: 370, height: 150)
+        HStack {
+            Image(profilePhoto)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100)
+                .clipShape(Circle())
+            VStack {
+                Text(name)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: 200, alignment: .leading)
+                Text(jobTitle + "\n" + "at " + company)
+                    .font(.subheadline)
+                    .frame(maxWidth: 200, alignment: .leading)
+                    .padding(.bottom, 5)
+                Text(industry)
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .background(.blue)
+                    .cornerRadius(5)
+                    .frame(maxWidth: 200, alignment: .leading)
+            }
+            Button {
+                isUnsavePopupVisible.toggle()
+            } label: {
+                Image(systemName: "bookmark.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20)
+                    .offset(x: -5, y: -25)
+            }
+        }
+    }
+}
+
+struct UnsavePopup: View {
+    var onOK: () -> Void
+    var onCancel: () -> Void
+
+    var body: some View {
+        VStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.white)
+                .frame(width: 180, height: 120)
+                .shadow(radius: 10, x: 5, y: 5)
+                .overlay(
+                    VStack {
+                        Text("Unsave profile?")
+                            .font(.headline)
+                            .padding()
+                        HStack {
+                            Button("OK  ") {
+                                onOK()
+                            }
+                            .foregroundColor(.blue)
+                            .cornerRadius(10)
+                            Button("  Cancel") {
+                                onCancel()
+                            }
+                            .foregroundColor(.red)
+                        }
+                        .padding(.bottom, 10)
+                    }
+                )
+                .position(x: 200, y: 360)
+        }
     }
 }
 
