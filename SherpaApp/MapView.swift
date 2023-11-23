d6e47cd79c966217d8085f32afd1dd00dd1866a4
Sunny Wong <sunnywong0207@gmail.com>
2023-11-23 19:18:20 +1100

    Adjust Toggle Position

--- SherpaApp/MapView.swift
+++ SherpaApp/MapView.swift
@@ -161,13 +161,14 @@ struct MapView: View {
         HeaderView()
         
         VStack(alignment:.leading){
-            Text("Auto Check-in")
             Toggle(isOn: $vibrateOnRing) {
             }
-                .offset(x:-340)
+                .offset(x:-343)
+            Text("Auto Check-in")
+                .font(.caption)
         }
         .frame(width:.infinity, height:200)
-        .offset(x:10,y:-240)
+        .offset(x:10,y:-245)
 
 
         if checkInList == true{
