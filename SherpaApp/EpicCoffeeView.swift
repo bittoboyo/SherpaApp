fe539ed10ff539b977d0e3ebb54ef02741dc10c1
Xuanxuanya-Z <zhanzhan126@gmail.com>
2023-11-23 23:58:35 +1100

    update on epic coffee

--- SherpaApp/EpicCoffeeView.swift
+++ SherpaApp/EpicCoffeeView.swift
@@ -16,7 +16,7 @@ struct EpicCoffeeView: View {
                     HStack{
                         Image(systemName: "figure.walk")
                             .font(.body)
-                        Text("15 mins walk")
+                        Text("4 mins walk")
                             .font(.body)
                             .frame(maxWidth: 360, alignment: .leading)
                     }
