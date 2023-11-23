9da698c840c0e60bbaabfb2794e057cb7a25ce81
Sunny Wong <sunnywong0207@gmail.com>
2023-11-23 12:32:42 +1100

    Revert "update on EpicCoffeeView"
    This reverts commit cae61a0c2c71e1457552295bea88ecd6734d9c18.

--- SherpaApp/PeopleView.swift
+++ SherpaApp/PeopleView.swift
@@ -70,8 +70,7 @@ struct PeopleView: View {
                                 skill2: "PM",
                                 description: "“The one thing I can’t live without is my cat Apollo, especially when I’m working on Figma and other tasks!”"
                             )
-                        }
-                        .foregroundStyle(.black)
+                        }.foregroundStyle(.black)
                         listPeople(
                             profilePhoto: "MaryP",
                             name: "Mary Pops",
@@ -154,7 +153,6 @@ func listPeople(profilePhoto: String,
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                         .frame(width: 80, height: 20)
-                        .padding(.horizontal,5)
                         .background(.blue)
                         .cornerRadius(5)
                 }
